f2ad333b9284fd670cb2eeff6acf3a83596b5b19
mnsrulz <manishrawat4u@gmail.com>
2024-11-19 21:35:12 -0500

    clean symbol

--- main.ts
+++ main.ts
@@ -129,7 +129,9 @@ router.get("/", (context) => {
 
         // console.log(`finding ${dt} in ${JSON.stringify(Object.keys(OptionsSnapshotSummaryLegacy))}`);
         if(Object.keys(OptionsSnapshotSummaryLegacy).includes(dt)){
-            const u = OptionsSnapshotSummaryLegacy[dt].symbols[decodeURIComponent(s)].dex.hdAssetUrl;
+            const cleanSymbol = decodeURIComponent(s).replace(/\W/g, '')
+            console.log(`Requested symbol: ${s}. Cleaned symbol: ${cleanSymbol}`);
+            const u = OptionsSnapshotSummaryLegacy[dt].symbols[decodeURIComponent(cleanSymbol)].dex.hdAssetUrl;
 
             //console.log(`asset url found: ${u}`);
             
