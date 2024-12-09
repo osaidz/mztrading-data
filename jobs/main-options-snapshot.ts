9e6633a8f675a22ad3cb1e9fded4fa97f4e3f965
mnsrulz <manishrawat4u@gmail.com>
2024-12-09 04:49:29 +0000

    using historical data for generating snapshots

--- jobs/main-options-snapshot.ts
+++ jobs/main-options-snapshot.ts
@@ -2,7 +2,7 @@ import { ensureDir } from "https://deno.land/std@0.224.0/fs/ensure_dir.ts";
 import { format } from "https://deno.land/std@0.224.0/datetime/format.ts";
 import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 import ky from "https://esm.sh/ky@1.2.3";
-import { getOptionsSnapshotSummary, ghRepoBaseUrl, cleanSymbol } from "../lib/data.ts";
+import { getOptionsDataSummary, getOptionsSnapshotSummary, ghRepoBaseUrl, cleanSymbol } from "../lib/data.ts";
 const dataFolder = `temp`;
 await ensureDir(dataFolder);
 const data = getOptionsSnapshotSummary();
@@ -16,87 +16,99 @@ data[releaseName] = {
 };
 const currentRelease = data[releaseName];
 
-const tickers = await ky("https://mztrading.netlify.app/api/watchlist").json<
-    { items: { symbol: string; name: string }[] }
->();
-
-console.log(`found ${tickers.items.length} tickers...`);
-const items = tickers.items; //.slice(0, 3); //for testing work only with 3 items
-for (const ticker of items) {
-    const { symbol } = ticker;
-    const cleanedSymbol = cleanSymbol(symbol)
-    console.log(`Fetching dex/gex page for ${symbol}`);
-
-    currentRelease.symbols[symbol] = {
-        dex: {
-            sdFileName: `${cleanedSymbol}_DEX_620.png`,
-            hdFileName: `${cleanedSymbol}_DEX_1240.png`,
-            sdAssetUrl: `${ghRepoBaseUrl}/${releaseName}/${cleanedSymbol}_DEX_620.png`,
-            hdAssetUrl: `${ghRepoBaseUrl}/${releaseName}/${cleanedSymbol}_DEX_1240.png`
-        },
-        gex: {
-            sdFileName: `${cleanedSymbol}_GEX_620.png`,
-            hdFileName: `${cleanedSymbol}_GEX_1240.png`,
-            sdAssetUrl: `${ghRepoBaseUrl}/${releaseName}/${cleanedSymbol}_GEX_620.png`,
-            hdAssetUrl: `${ghRepoBaseUrl}/${releaseName}/${cleanedSymbol}_GEX_1240.png`
-        }
-    }
-
-    const currentSymbol = currentRelease.symbols[symbol];
-
+const dataSummary = getOptionsDataSummary()
+const latestObject = Object.keys(dataSummary).pop() || '';
+const latestDate = dataSummary[latestObject].displayName
+if (latestDate) {
+    const allSymbols = Object.keys(dataSummary[latestObject].symbols);//.slice(0, 30); //for testing work only with 3 items
+    console.log(`found ${allSymbols.length} tickers...`);
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({ width: 620, height: 620, deviceScaleFactor: 2 }); // set the viewport size
+    const page = await browser.newPage();    
     await page.goto(
-        `https://mztrading.netlify.app/options/analyze/${symbol}/dh?showDexGex=true&dgextab=DEX&print=true`,
+        `https://mztrading.netlify.app/tools/snapshot?showDexGex=true&dgextab=DEX&print=true&datamode=${encodeURIComponent(latestDate)}`,
         {
             waitUntil: "networkidle2",
         },
     ); // replace
 
-    console.log(`Generating high definition DEX snapshot page for ${symbol}`);
-    
-    await page.waitForNetworkIdle();    
-    await page.screenshot({
-        path: `${dataFolder}/${currentSymbol.dex.hdFileName}`,
-    }); // take a screenshot and save it to a file
-    
-    console.log(`Generating standard definition DEX snapshot page for ${symbol}`);
-    await page.setViewport({ width: 620, height: 620, deviceScaleFactor: 1 }); // set the viewport size
-    
-    await page.waitForNetworkIdle();
-    await page.screenshot({
-        path: `${dataFolder}/${currentSymbol.dex.sdFileName}`,
-    }); // take a screenshot and save it to a file
-    
-    await page.evaluate(`
-        const url = new URL(location.href);
-        url.searchParams.set("dgextab", "GEX");
-        history.replaceState(null, "", url);
-        `);        
-    console.log(`Generating standard definition GEX snapshot page for ${symbol}`);
-    
-    await page.waitForNetworkIdle();
-    await page.screenshot({
-        path: `${dataFolder}/${currentSymbol.gex.sdFileName}`,
-    }); // take a screenshot and save it to a file
-    
-    await page.setViewport({ width: 620, height: 620, deviceScaleFactor: 2 }); // set the viewport size
-    console.log(`Generating high definition GEX snapshot page for ${symbol}`);
+        
+    for (const symbol of allSymbols) {
+        const cleanedSymbol = cleanSymbol(symbol)
+        console.log(`Fetching dex/gex page for ${symbol}`);
 
-    await page.waitForNetworkIdle();
-    await page.screenshot({
-        path: `${dataFolder}/${currentSymbol.gex.hdFileName}`,
-    }); // take a screenshot and save it to a file
+        currentRelease.symbols[symbol] = {
+            dex: {
+                sdFileName: `${cleanedSymbol}_DEX_620.png`,
+                hdFileName: `${cleanedSymbol}_DEX_1240.png`,
+                sdAssetUrl: `${ghRepoBaseUrl}/${releaseName}/${cleanedSymbol}_DEX_620.png`,
+                hdAssetUrl: `${ghRepoBaseUrl}/${releaseName}/${cleanedSymbol}_DEX_1240.png`
+            },
+            gex: {
+                sdFileName: `${cleanedSymbol}_GEX_620.png`,
+                hdFileName: `${cleanedSymbol}_GEX_1240.png`,
+                sdAssetUrl: `${ghRepoBaseUrl}/${releaseName}/${cleanedSymbol}_GEX_620.png`,
+                hdAssetUrl: `${ghRepoBaseUrl}/${releaseName}/${cleanedSymbol}_GEX_1240.png`
+            }
+        }
 
-    await browser.close();
+        const currentSymbol = currentRelease.symbols[symbol];
+        
+        await page.setViewport({ width: 620, height: 620, deviceScaleFactor: 2 }); // set the viewport size
+        let varurlname = `urldex${allSymbols.indexOf(symbol)}`
+        await page.evaluate(`
+            let ${varurlname} = new URL(location.href);
+            ${varurlname}.searchParams.set("dgextab", "DEX");
+            ${varurlname}.searchParams.set("symbol", "${symbol}");
+            history.replaceState(null, "", ${varurlname});
+        `);
+
+        console.log(`Generating high definition DEX snapshot page for ${symbol}`);
+
+        await page.waitForNetworkIdle();
+        await page.screenshot({
+            path: `${dataFolder}/${currentSymbol.dex.hdFileName}`
+        }); // take a screenshot and save it to a file
+
+        console.log(`Generating standard definition DEX snapshot page for ${symbol}`);
+        await page.setViewport({ width: 620, height: 620, deviceScaleFactor: 1 }); // set the viewport size
+        
+        await page.waitForNetworkIdle();
+        await page.screenshot({
+            path: `${dataFolder}/${currentSymbol.dex.sdFileName}`,
+        }); // take a screenshot and save it to a file
+        
+        varurlname = `urlgex${allSymbols.indexOf(symbol)}`
+        await page.evaluate(`
+        let ${varurlname} = new URL(location.href);
+        ${varurlname}.searchParams.set("dgextab", "GEX");
+        history.replaceState(null, "", ${varurlname});
+        `);
+        console.log(`Generating standard definition GEX snapshot page for ${symbol}`);
+
+        await page.waitForNetworkIdle();
+        await page.screenshot({
+            path: `${dataFolder}/${currentSymbol.gex.sdFileName}`,
+        }); // take a screenshot and save it to a file
+
+        await page.setViewport({ width: 620, height: 620, deviceScaleFactor: 2 }); // set the viewport size
+        console.log(`Generating high definition GEX snapshot page for ${symbol}`);
+
+        await page.waitForNetworkIdle();
+        await page.screenshot({
+            path: `${dataFolder}/${currentSymbol.gex.hdFileName}`,
+        }); // take a screenshot and save it to a file
+
+        // await browser.close();
+    }
+
+    console.log(`Finished generating snapshot files!`);
+
+    Deno.writeTextFileSync(
+        "./data/options-snapshot.summary.json",
+        JSON.stringify(data),
+    );
+
+    console.log(`Summary file generated successfully!`);
+} else {
+    console.log(`Unable to find any latest date in the data summary file!`);
 }
-
-console.log(`Finished generating snapshot files!`);
-
-Deno.writeTextFileSync(
-    "./data/options-snapshot.summary.json",
-    JSON.stringify(data),
-);
-
-console.log(`Summary file generated successfully!`);
