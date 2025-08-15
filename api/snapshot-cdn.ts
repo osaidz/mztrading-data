5b9c7256bdf44e25f943af6f6937d4b6a6758ae2
mnsrulz <manishrawat4u@gmail.com>
2025-08-14 23:31:45 -0400

    minor fix

--- api/snapshot-cdn.ts
+++ api/snapshot-cdn.ts
@@ -11,33 +11,27 @@ import { getZipAssetUrlForSymbol, zipServiceUrl } from "../lib/data.ts";
 const cache = await caches.open("default");
 
 Deno.serve(async (req) => {
-    if (req.url) {
-        //check if the url is api/snapshots and parse the query params
-        const url = new URL(req.url);
-        if (url.pathname === "/api/snapshots") {
-            const dt = url.searchParams.get("dt");
-            const symbol = url.searchParams.get("symbol");
-            const f = url.searchParams.get("f");
-            if (symbol && dt && f) {
-                const zipAssetUrl = getZipAssetUrlForSymbol(symbol, dt);
-                const assetUrl = `${zipServiceUrl}?f=${f}&q=${zipAssetUrl}`;
-                const cached = await cache.match(assetUrl);
-                if (cached) {
-                    return cached;
-                }
-                const res = await fetch(assetUrl);
-                if (!res.ok) return res;
-
-                await cache.put(assetUrl, res.clone());
-                return res;
+    //check if the url is api/snapshots and parse the query params
+    const url = new URL(req.url);
+    if (url.pathname === "/api/snapshots") {
+        const dt = url.searchParams.get("dt");
+        const symbol = url.searchParams.get("symbol");
+        const f = url.searchParams.get("f");
+        if (symbol && dt && f) {
+            const zipAssetUrl = getZipAssetUrlForSymbol(symbol, dt);
+            const assetUrl = `${zipServiceUrl}?f=${f}&q=${zipAssetUrl}`;
+            const cached = await cache.match(assetUrl);
+            if (cached) {
+                return cached;
             }
-        }
-        return new Response("Not Found", { status: 404 });
-    }
+            const res = await fetch(assetUrl);
+            if (!res.ok) return res;
 
-    const res = new Response("cached at " + new Date().toISOString());
-    await cache.put(req, res.clone());
-    return res;
+            await cache.put(assetUrl, res.clone());
+            return res;
+        }
+    }
+    return new Response("Not Found", { status: 404 });
 });
 
 // router.get("/api/snapshots", async (context) => {
