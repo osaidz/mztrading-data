4e2e0b9301f3af123e0250d6c32745ff2ad60a21
mnsrulz <manishrawat4u@gmail.com>
2025-08-21 05:28:36 +0000

    expose cachekeys

--- api/snapshot-cdn.ts
+++ api/snapshot-cdn.ts
@@ -1,29 +1,44 @@
 import { getZipAssetUrlForSymbol, zipServiceUrl } from "../lib/data.ts";
 const cache = await caches.open("default");
+import { Hono } from 'https://esm.sh/hono'
 
-Deno.serve(async (req) => {    
-    const url = new URL(req.url);
-    if (url.pathname === "/api/snapshots") {
-        const dt = url.searchParams.get("dt");
-        const symbol = url.searchParams.get("symbol");
-        const f = url.searchParams.get("f");
-        if (symbol && dt && f) {
-            const zipAssetUrl = getZipAssetUrlForSymbol(symbol, dt);
-            const assetUrl = `${zipServiceUrl}?f=${f}&q=${zipAssetUrl}`;
-            const cached = await cache.match(assetUrl);
-            if (cached) {
-                console.log(`Serving from cache. symbol=${symbol}, dt=${dt}, f=${f}`);
-                return cached;
-            }
+const app = new Hono()
 
-            console.log(`Fetching from network. symbol=${symbol}, dt=${dt}, f=${f}`);
-            const res = await fetch(assetUrl);
-            if (!res.ok) return res;           
-
-            await cache.put(assetUrl, res.clone());
-            return res;
+app.use('/api/snapshots', async ({ req }) => {
+    const dt = req.query('dt');;
+    const symbol = req.query('symbol');
+    const f = req.query('f');
+    if (symbol && dt && f) {
+        const zipAssetUrl = getZipAssetUrlForSymbol(symbol, dt);
+        const assetUrl = `${zipServiceUrl}?f=${f}&q=${zipAssetUrl}`;
+        const cached = await cache.match(assetUrl);
+        if (cached) {
+            console.log(`Serving from cache. symbol=${symbol}, dt=${dt}, f=${f}`);
+            return cached;
         }
-        return new Response("Not Found", { status: 404 });
-    }
-    return new Response("/api/snapshots?symbol=<symbol>&dt=<dt>&f=<f>");
+
+        console.log(`Fetching from network. symbol=${symbol}, dt=${dt}, f=${f}`);
+        const res = await fetch(assetUrl);
+        if (!res.ok) return res;
+
+        await cache.put(assetUrl, res.clone());
+        return res;
+    }    
+    return new Response("Use /api/snapshots?symbol=<symbol>&dt=<dt>&f=<f> to fetch snapshot files.");
 });
+
+app.use('/api/cachekeys', async ()=> {
+    const allKeys = await cache.keys();
+    return new Response(allKeys);
+});
+
+Deno.serve(app.fetch)
+
+// Deno.serve(async (req) => {    
+//     const url = new URL(req.url);
+//     if (url.pathname === "/api/snapshots") {
+//         }
+//         return new Response("Not Found", { status: 404 });
+//     }
+//     return new Response("/api/snapshots?symbol=<symbol>&dt=<dt>&f=<f>");
+// });
